5c698d8e806a6f4b9ba20b904adfb98b864c8f16
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-19 10:00:01 +0700

    Election.Analytic.System.v2 - PPRP  #6 - NLib.Reports project   - Update App.Config related to Report Viewer.

---
+++ 01.Core/PPRP.Core/Consts/AppConsts.cs
@@ -0,0 +1,38 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace PPRP
+{
+    public static class AppConsts
+    {
+        // common properties
+        public static string Version = "1";
+        public static string Minor = "1";
+
+        public static class Application
+        {
+            public static class Analytic
+            {
+                public static string ApplicationName = @"PPRP Analytic Application";
+                // common
+                public static string Version = AppConsts.Version;
+                public static string Minor = AppConsts.Minor;
+                public static string Build = "800";
+                public static DateTime LastUpdate = new DateTime(2022, 11, 19, 10, 00, 00);
+            }
+
+            public static class Management
+            {
+                public static string ApplicationName = @"PPRP Management Application";
+                // common
+                public static string Version = AppConsts.Version;
+                public static string Minor = AppConsts.Minor;
+                public static string Build = "800";
+                public static DateTime LastUpdate = new DateTime(2022, 11, 19, 10, 00, 00);
+            }
+        }
+    }
+}
---
+++ 01.Core/PPRP.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PPRP Core Library")]
+[assembly: AssemblyDescription("The PPRP Core assembly for Election Analytic system.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Softbase Co., Ltd.")]
+[assembly: AssemblyProduct("The PPRP Core assembly for Election Analytic system.")]
+[assembly: AssemblyCopyright("Copyright ©Softbase Co., Ltd. 2022")]
+[assembly: AssemblyTrademark("The PPRP Core assembly for PPRP Election Analytic system.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25f76701-c7d6-4cc4-beb9-db40d2bbbd9d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
