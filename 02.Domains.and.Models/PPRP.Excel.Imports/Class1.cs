b1c9b679b5370e407d668b8ff8abb3816ce79a31
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-19 10:51:23 +0700

    Election.Analytic.System.v2 - PPRP  #9 - Port PPRP.Excel.Imports project from v1   - update all reference dlls

---
+++ 02.Domains.and.Models/PPRP.Excel.Imports/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPRP.Excel.Imports
+{
+    public class Class1
+    {
+    }
+}
