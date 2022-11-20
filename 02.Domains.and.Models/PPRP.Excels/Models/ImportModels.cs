7916d03ecfe3e599f0ca7b04c156dd754ba92c28
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-20 21:03:13 +0700

    Election.Analytic.System.v2 - PPRP  #20 - Add Wpf.Lambda.Sample project.

---
+++ 02.Domains.and.Models/PPRP.Excels/Models/ImportModels.cs
@@ -0,0 +1,45 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OfficeOpenXml;
+
+#endregion
+
+namespace PPRP.Models
+{
+    public class ExcelImport
+    {
+        #region Constructor (Static)
+
+        static ExcelImport() 
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ExcelImport() : base() { }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~ExcelImport() { }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Map() { }
+
+        #endregion
+    }
+}
