290d985a44ca63e59d0d22ff704e277de2793f16
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-23 22:37:06 +0700

    Election.Analytic.System.v2 - PPRP  #44 - Implements Wpf.Lambda.Sample project #19.   - Test Generic property.

---
+++ 20.Samples/Wpf.Lambda.Sample/Caches.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf.Lambda.Sample
+{
+    public class Property<T> 
+    { 
+        public T Value { get; set; }
+    }
+    public class Caches
+    {
+        private static Caches _instance = null;
+        
+        public static Caches Instance
+        {
+            get 
+            {
+                if (null == _instance)
+                {
+                    lock (typeof(Caches))
+                    {
+                        _instance = new Caches();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public T Of<T>()
+        {
+            return default;
+        }
+    }
+
+    public class Test
+    {
+        public void Setup()
+        {
+            string name = Caches.Instance.Of<Test>().FullName;
+            if (null != name) return;
+        }
+
+        public string FullName { get; set; }
+    }
+}
