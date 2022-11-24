294a982d177ecff63c3d7e05cee20dc28b0f6160
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-24 21:31:55 +0700

    Election.Analytic.System.v2 - PPRP  #51 - PPRP.Domains project   - Implements Person & Party classes #1.

---
+++ 02.Domains.and.Models/PPRP.Domains/Domains/Parson.And.Party/MParty.cs
@@ -0,0 +1,61 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace PPRP.Domains
+{
+    #region  MPerson
+
+    /// <summary>
+    /// The MPerson class.
+    /// </summary>
+    public class MParty : NInpc
+    {
+        #region Internal Variables
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MParty() : base()
+        {
+
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~MParty()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+
+        #region Static Methods
+
+        #endregion
+    }
+
+    #endregion
+}
---
+++ 02.Domains.and.Models/PPRP.Domains/Domains/Parson.And.Party/MPerson.cs
@@ -0,0 +1,61 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace PPRP.Domains
+{
+    #region  MPerson
+
+    /// <summary>
+    /// The MPerson class.
+    /// </summary>
+    public class MPerson : NInpc
+    {
+        #region Internal Variables
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MPerson() : base()
+        {
+
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~MPerson()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+
+        #region Static Methods
+
+        #endregion
+    }
+
+    #endregion
+}
