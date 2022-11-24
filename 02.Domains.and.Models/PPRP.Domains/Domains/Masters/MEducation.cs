1fd2042311300018bc0acadf545f5ced3f43de42
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-24 21:38:00 +0700

    Election.Analytic.System.v2 - PPRP  #52 - PPRP.Domains project   - Implements Masters classes #1.

---
+++ 02.Domains.and.Models/PPRP.Domains/Domains/Masters/MEducation.cs
@@ -0,0 +1,31 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace PPRP.Domains
+{
+    #region MEducation
+
+    /// <summary>
+    /// The MEducation class.
+    /// </summary>
+    public class MEducation
+    {
+    }
+
+    #endregion
+}
---
+++ 02.Domains.and.Models/PPRP.Domains/Domains/Masters/MOccupation.cs
@@ -0,0 +1,31 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace PPRP.Domains
+{
+    #region MOccupation
+
+    /// <summary>
+    /// The MOccupation class.
+    /// </summary>
+    public class MOccupation
+    {
+    }
+
+    #endregion
+}
