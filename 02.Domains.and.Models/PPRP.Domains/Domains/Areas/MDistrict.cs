f0935eb0a4771c33dc2391c05feb261a4d6894d3
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-27 23:39:51 +0700

    Election.Analytic.System.v2 - PPRP  #65 - PPRP Domain and Models implements #8   - Implements MADM2 class #1.

--- 02.Domains.and.Models/PPRP.Domains/Domains/Areas/MDistrict.cs
+++ 02.Domains.and.Models/PPRP.Domains/Domains/Areas/MDistrict.cs
@@ -18,12 +18,53 @@ using Newtonsoft.Json;
 
 namespace PPRP.Domains
 {
+    #region MADM2
+
+    /// <summary>
+    /// The MADM2 class.
+    /// </summary>
+    public class MADM2 : NInpc
+    {
+        #region Internal Variables
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MADM2() : base()
+        {
+
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~MADM2()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+
+        #region Static Methods
+
+        #endregion
+    }
+
+    #endregion
+
     #region MDistrict
 
     /// <summary>
     /// The MDistrict class.
     /// </summary>
-    public class MDistrict : NInpc
+    public class MDistrict : MADM2
     {
         #region Internal Variables
 
