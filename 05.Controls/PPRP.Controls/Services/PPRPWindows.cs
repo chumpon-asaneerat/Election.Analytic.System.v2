180dc9e94a96d6e0032abf17ae9957c4315e20c2
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-29 01:32:20 +0700

    Election.Analytic.System.v2 - PPRP  #73 - Implements Signin process #3.   - Add MessageBox, MessageBoxOKCancel Windows #1.

---
+++ 05.Controls/PPRP.Controls/Services/PPRPWindows.cs
@@ -0,0 +1,58 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using PPRP.Windows;
+
+#endregion
+
+namespace PPRP
+{
+    public static class PPRPWindows
+    {
+        public static class Windows
+        {
+            #region Application Main Window
+
+            /// <summary>Gets Application Main Window.</summary>
+            public static Window MainWindow { get { return Application.Current.MainWindow; } }
+
+            #endregion
+
+            #region MessageBox
+
+            /// <summary>Gets MessageBox Window.</summary>
+            public static MessageBoxWindow MessageBox
+            {
+                get
+                {
+                    var ret = new PPRP.Windows.MessageBoxWindow();
+                    ret.Owner = Application.Current.MainWindow;
+                    return ret;
+                }
+            }
+
+            #endregion
+
+            #region MessageBoxOKCancel
+
+            /// <summary>Gets MessageBoxOkCancel Window.</summary>
+            public static MessageBoxOKCancelWindow MessageBoxOKCancel
+            {
+                get
+                {
+                    var ret = new PPRP.Windows.MessageBoxOKCancelWindow();
+                    ret.Owner = Application.Current.MainWindow;
+                    return ret;
+                }
+            }
+
+            #endregion
+        }
+    }
+}
