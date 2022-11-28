af82e821f26bdb86d5ffe53786fe9dde44b33f11
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-29 05:23:19 +0700

    Election.Analytic.System.v2 - PPRP  #87 - PPRP.Manangement.App project   - Implements UI #2.

---
+++ 09.App/PPRP.Manangement.App/Pages/MPD2562PollingUnitManagePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPRP.Pages
+{
+    /// <summary>
+    /// Interaction logic for MPD2562PollingUnitManagePage.xaml
+    /// </summary>
+    public partial class MPD2562PollingUnitManagePage : UserControl
+    {
+        public MPD2562PollingUnitManagePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
