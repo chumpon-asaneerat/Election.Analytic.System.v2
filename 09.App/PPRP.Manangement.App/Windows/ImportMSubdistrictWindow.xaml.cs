c4d5abecaddf7e1b8c0b0078b8ea05df85355678
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-29 06:36:22 +0700

    Election.Analytic.System.v2 - PPRP  #95 - PPRP.Manangement.App project   - Implements UI #9.

---
+++ 09.App/PPRP.Manangement.App/Windows/ImportMSubdistrictWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+using NLib;
+using NLib.Reflection;
+using NLib.Services;
+
+using PPRP.Models;
+using PPRP.Models.Excel;
+
+#endregion
+
+namespace PPRP.Windows
+{
+    /// <summary>
+    /// Interaction logic for ImportMSubdistrictWindow.xaml
+    /// </summary>
+    public partial class ImportMSubdistrictWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ImportMSubdistrictWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            //import.OnSampleDataChanged += Import_OnSampleDataChanged;
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            //import.OnSampleDataChanged -= Import_OnSampleDataChanged;
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void cmdFinish_Click(object sender, RoutedEventArgs e)
+        {
+            Imports();
+            DialogResult = true;
+        }
+
+        private void cmdChooseExcel_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseExcelFile();
+        }
+
+        #endregion
+
+        #region NExcelImport Handlers
+
+        private void Import_OnSampleDataChanged(object sender, EventArgs e)
+        {
+            /*
+            if (null != wsMap && null != wsMap.ImportModel)
+            {
+                var model = wsMap.ImportModel;
+                lvMapPreview.Setup(import);
+
+                items = XlsMProvince.LoadWorksheetTable(import, model.Worksheet.SheetName, model.Maps);
+                if (null != items)
+                {
+
+                }
+                lvMapPreview.UpdateItems(model.Maps, items);
+            }
+            */
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ChooseExcelFile()
+        {
+
+        }
+
+        private void Imports()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Setup.
+        /// </summary>
+        public void Setup()
+        {
+
+        }
+
+        #endregion
+    }
+}
