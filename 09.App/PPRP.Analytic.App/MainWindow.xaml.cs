53422c6eb5133ea8893335f2cd68b2e9f0f6e760
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-28 13:33:04 +0700

    Election.Analytic.System.v2 - PPRP  #68 - Port PPRP.Analytic.App project from v1   - remove all code.

---
+++ 09.App/PPRP.Analytic.App/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+using NLib.Services;
+using PPRP.Services;
+
+#endregion
+
+namespace PPRP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Start Server
+            DbServer.Instance.Start();
+
+            #region Test Connection
+            /*
+            var rets = Domains.MTitle.Gets();
+            if (null != rets)
+            {
+                Console.WriteLine("Count: {0}", rets.Value.Count);
+            }
+            */
+            #endregion
+
+            // Initial Page Content Manager
+            PageContentManager.Instance.ContentChanged += new EventHandler(Instance_ContentChanged);
+            PageContentManager.Instance.Start();
+
+            // Init SignIn Manager
+            SignInManager.Instance.UserChanged += Instance_UserChanged;
+
+            // Sign In.
+            var page = PPRPApp.Pages.SignIn;
+            page.Setup();
+            PageContentManager.Instance.Current = page;
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            // Release SignIn Manager
+            SignInManager.Instance.Signout();
+            SignInManager.Instance.UserChanged -= Instance_UserChanged;
+
+            // Release Page Content Manager
+            PageContentManager.Instance.Shutdown();
+            PageContentManager.Instance.ContentChanged -= new EventHandler(Instance_ContentChanged);
+
+            // Shutdown Server
+            DbServer.Instance.Shutdown();
+        }
+
+        #endregion
+
+        #region Page Content Manager Handlers
+
+        void Instance_ContentChanged(object sender, EventArgs e)
+        {
+            this.container.Content = PageContentManager.Instance.Current;
+        }
+
+        #endregion
+
+        #region SignIn Manager Handlers
+
+        private void Instance_UserChanged(object sender, EventArgs e)
+        {
+            if (null == SignInManager.Instance.User)
+            {
+                // Signout - show Sign In page.
+                var page = PPRPApp.Pages.SignIn;
+                page.Setup();
+                PageContentManager.Instance.Current = page;
+            }
+            else
+            {
+
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ 09.App/PPRP.Analytic.App/Services/PPRPApp.cs
@@ -0,0 +1,79 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using PPRP.Pages;
+
+#endregion
+
+namespace PPRP
+{
+    /// <summary>
+    /// The PPRPApp class.
+    /// </summary>
+    public static class PPRPApp
+    {
+        /// <summary>
+        /// Variables Static class.
+        /// </summary>
+        public static class Variables
+        {
+            /*
+            /// <summary>Chief Revenue Entry Prerender DateTime.</summary>
+            public static DateTime ChiefRevenueLastRenderTime = DateTime.MinValue;
+            /// <summary>Collector Revenue Entry Prerender DateTime.</summary>
+            public static DateTime CollectorRevenueLastRenderTime = DateTime.MinValue;
+            */
+        }
+        /// <summary>
+        /// Permissions Static class.
+        /// </summary>
+        public static class Permissions
+        {
+        }
+        /// <summary>
+        /// Pages Static class.
+        /// </summary>
+        public static class Pages
+        {
+            #region SignIn
+
+            private static SignInPage _SignIn;
+
+            /// <summary>Gets Sign In Page.</summary>
+            public static SignInPage SignIn
+            {
+                get
+                {
+                    if (null == _SignIn)
+                    {
+                        lock (typeof(PPRPApp))
+                        {
+                            _SignIn = new SignInPage();
+                        }
+                    }
+                    return _SignIn;
+                }
+            }
+
+            #endregion
+        }
+        /// <summary>
+        /// Windows Static class.
+        /// </summary>
+        public static class Windows
+        {
+            #region Application Main Window
+
+            /// <summary>Gets Application Main Window.</summary>
+            public static Window MainWindow { get { return Application.Current.MainWindow; } }
+
+            #endregion
+        }
+    }
+}
