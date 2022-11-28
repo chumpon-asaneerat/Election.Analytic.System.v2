3d5be58a3d062db7152e7d53e3b0a96f5c16cefc
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-29 06:21:15 +0700

    Election.Analytic.System.v2 - PPRP  #94 - PPRP.Manangement.App project   - Implements UI #8.

---
+++ 05.Controls/PPRP.Controls/Controls/Excels/ExcelColumnMapControl.xaml.cs
@@ -0,0 +1,104 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+using NLib;
+using NLib.Reflection;
+using NLib.Services;
+using PPRP.Models;
+using PPRP.Models.Excel;
+
+#endregion
+
+namespace PPRP.Controls.Excels
+{
+    /// <summary>
+    /// Interaction logic for ExcelColumnMapControl.xaml
+    /// </summary>
+    public partial class ExcelColumnMapControl : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ExcelColumnMapControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Combobox Handlers
+
+        private void cbSheets_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            /*
+            var item = cbSheets.SelectedItem as NExcelWorksheet;
+            LoadSheetColumns(item);
+            */
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdResetMapProperty_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var ctx = (null != button) ? button.DataContext : null;
+            /*
+            var map = (null != ctx) ? ctx as NExcelMapProperty : null;
+            if (null != map)
+            {
+                map.SelectedColumn = null; // reset
+            }
+            */
+        }
+        private void cmdLoadExcelData_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            if (null == _import)
+                return;
+            _import.RaiseSampleDataChanged();
+            */
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+    }
+}
---
+++ 05.Controls/PPRP.Controls/Controls/Excels/ExcelDataViewControl.xaml.cs
@@ -0,0 +1,75 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+using NLib;
+using NLib.Reflection;
+using NLib.Services;
+using PPRP.Models;
+using PPRP.Models.Excel;
+
+#endregion
+
+namespace PPRP.Controls.Excels
+{
+    /// <summary>
+    /// Interaction logic for ExcelDataViewControl.xaml
+    /// </summary>
+    public partial class ExcelDataViewControl : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ExcelDataViewControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region ListBox Handlers
+
+        #endregion
+
+        #region Button Handlers
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+    }
+}
