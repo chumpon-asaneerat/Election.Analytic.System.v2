db82b1fbf435f6a34289fecfab146ee1aeec19a6
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-02 20:54:15 +0700

    Election.Analytic.System.v2 - PPRP  #198 - PPRP.Manangement.App project   - Implements UI #96.

---
+++ 09.App/PPRP.Manangement.App/Windows/ImportPersonImageWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+using NLib;
+using NLib.Reflection;
+using NLib.Services;
+
+using PPRP.Models;
+
+#endregion
+
+namespace PPRP.Windows
+{
+    /// <summary>
+    /// Interaction logic for ImportPersonImageWindow.xaml
+    /// </summary>
+    public partial class ImportPersonImageWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ImportPersonImageWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private int iPageNo = 1;
+        private int iMaxPage = 1;
+        private int iRowsPerPage = 40;
+
+        #endregion
+
+        #region Button Handlers
+
+        #region Cancel/Finish
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void cmdFinish_Click(object sender, RoutedEventArgs e)
+        {
+            if (Import())
+            {
+                DialogResult = true;
+            }
+        }
+
+        private void cmdChooseFolder_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseImageFolder();
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Paging Handlers
+
+        private void nav_PagingChanged(object sender, EventArgs e)
+        {
+            iPageNo = nav.PageNo;
+            RefreshList();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private bool Import()
+        {
+
+            return true;
+        }
+
+        private void ChooseImageFolder()
+        {
+
+        }
+
+        private void RefreshList()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Setup
+        /// </summary>
+        public void Setup()
+        {
+            iPageNo = 1;
+            iMaxPage = 1;
+
+            RefreshList();
+        }
+
+        #endregion
+    }
+}
