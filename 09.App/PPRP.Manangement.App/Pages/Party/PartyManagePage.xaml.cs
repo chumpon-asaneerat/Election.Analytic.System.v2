77a15561b41603c52b324bea1baffca0453d38d7
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-02 20:16:01 +0700

    Election.Analytic.System.v2 - PPRP  #193 - PPRP.Manangement.App project   - Implements UI #91.

--- 09.App/PPRP.Manangement.App/Pages/Party/PartyManagePage.xaml.cs
+++ 09.App/PPRP.Manangement.App/Pages/Party/PartyManagePage.xaml.cs
@@ -33,6 +33,15 @@ namespace PPRP.Pages
 
         #endregion
 
+        #region Internal Variables
+
+        private string sPartyNameFilter = string.Empty;
+        private int iPageNo = 1;
+        private int iMaxPage = 1;
+        private int iRowsPerPage = 40;
+
+        #endregion
+
         #region Button Handlers
 
         private void cmdAddNew_Click(object sender, RoutedEventArgs e)
@@ -70,6 +79,61 @@ namespace PPRP.Pages
             Print();
         }
 
+        private void cmdEdit_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            var btn = sender as Button;
+            if (null == btn) return;
+            var item = btn.DataContext as MParty;
+            Edit(item);
+            */
+        }
+
+        private void cmdDelete_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            var btn = sender as Button;
+            if (null == btn) return;
+            var item = btn.DataContext as MParty;
+            Delete(item);
+            */
+        }
+
+        #endregion
+
+        #region TextBox Handlers
+
+        private void txtPartyNameFilter_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            /*
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                e.Handled = true; // mark as handled
+                // search
+                Search();
+            }
+            else if (e.Key == System.Windows.Input.Key.Escape)
+            {
+                e.Handled = true; // mark as handled
+                // reset filter and search
+                txtPartyNameFilter.Text = string.Empty;
+                Search();
+            }
+            */
+        }
+
+        #endregion
+
+        #region Paging Handlers
+
+        private void nav_PagingChanged(object sender, EventArgs e)
+        {
+            /*
+            iPageNo = nav.PageNo;
+            RefreshList();
+            */
+        }
+
         #endregion
 
         #region Private Methods
@@ -83,7 +147,15 @@ namespace PPRP.Pages
 
         private void Import()
         {
-
+            /*
+            var win = PPRPApp.Windows.ImportPartyImage;
+            win.Setup();
+            if (win.ShowDialog() == false)
+            {
+                return;
+            }
+            RefreshList();
+            */
         }
 
         private void Export()
@@ -98,12 +170,63 @@ namespace PPRP.Pages
 
         private void Search()
         {
-
+            /*
+            var win = PPRPApp.Windows.ImportPartyImage;
+            win.Setup();
+            if (win.ShowDialog() == false)
+            {
+                return;
+            }
+            RefreshList();
+            */
         }
 
         private void Print()
         {
 
+        }
+
+        private void Edit(MParty item)
+        {
+            /*
+            if (null == item)
+                return;
+            Console.WriteLine("Edit");
+            */
+        }
+
+        private void Delete(MParty item)
+        {
+            /*
+            if (null == item)
+                return;
+            string msg = string.Format("ต้องการลบข้อมูล '{0}' ?", item.PartyName);
+            if (MessageBox.Show(msg, "ยืนยันการลบข้อมูล", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                MParty.Delete(item);
+                RefreshList();
+            }
+            */
+        }
+
+        private void RefreshList()
+        {
+            /*
+            lvParties.ItemsSource = null;
+            var parties = MParty.Gets(sPartyNameFilter, iPageNo, iRowsPerPage);
+            lvParties.ItemsSource = (null != parties) ? parties.Value : new List<MParty>();
+
+            if (null != parties)
+            {
+                lvParties.SelectedIndex = 0;
+                lvParties.ScrollIntoView(lvParties.SelectedItem);
+            }
+
+            iPageNo = (null != parties) ? parties.PageNo : 1;
+            iMaxPage = (null != parties) ? parties.MaxPage : 1;
+
+            nav.Setup(iPageNo, iMaxPage);
+            */
         }
 
         #endregion
@@ -118,7 +241,13 @@ namespace PPRP.Pages
         {
             if (reload)
             {
+                /*
+                sPartyNameFilter = string.Empty;
+                iPageNo = 1;
+                iMaxPage = 1;
 
+                RefreshList();
+                */
             }
         }
 
