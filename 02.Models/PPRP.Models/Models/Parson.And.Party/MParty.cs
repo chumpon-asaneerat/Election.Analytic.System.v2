72ce9f6c5e2abf5d6dacf26273b9c04f7129750c
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-03 17:01:49 +0700

    Election.Analytic.System.v2 - PPRP  #215 - PPRP.Manangement.App project   - Implements UI #109.

--- 02.Models/PPRP.Models/Models/Parson.And.Party/MParty.cs
+++ 02.Models/PPRP.Models/Models/Parson.And.Party/MParty.cs
@@ -18,14 +18,21 @@ using Newtonsoft.Json;
 
 namespace PPRP.Models
 {
-    #region  MPerson
+    #region  MParty
 
     /// <summary>
-    /// The MPerson class.
+    /// The MParty class.
     /// </summary>
     public class MParty : NInpc
     {
         #region Internal Variables
+
+        private int _PartyId = 0;
+        private string _PartyName = null;
+
+        private bool _isDefault = true;
+        private bool _ImageLoading = false;
+        private ImageSource _img = null;
 
         #endregion
 
@@ -50,9 +57,153 @@ namespace PPRP.Models
 
         #region Public Properties
 
+        /// <summary>
+        /// Gets or sets PartyId.
+        /// </summary>
+        public int PartyId
+        {
+            get { return _PartyId; }
+            set
+            {
+                if (_PartyId != value)
+                {
+                    _PartyId = value;
+                    // Raise Event
+                    Raise(() => PartyId);
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets Party Name.
+        /// </summary>
+        public string PartyName
+        {
+            get { return _PartyName; }
+            set
+            {
+                if (_PartyName != value)
+                {
+                    _PartyName = value;
+                    // Raise Event
+                    Raise(() => PartyName);
+                }
+            }
+        }
+        /// <summary>
+        /// Gets or sets Data.
+        /// </summary>
+        public byte[] Data { get; set; }
+        /// <summary>
+        /// Gets Image.
+        /// </summary>
+        public ImageSource Image
+        {
+            get
+            {
+                if (null == _img && !_ImageLoading)
+                {
+                    _ImageLoading = true;
+
+                    Defaults.RunInBackground(() =>
+                    {
+                        ImageSource imgSrc;
+                        if (null == Data)
+                        {
+                            _isDefault = true;
+                            imgSrc = Defaults.Person;
+                        }
+                        else
+                        {
+                            _isDefault = false;
+                            imgSrc = ByteUtils.GetImageSource(Data);
+                        }
+                        _img = imgSrc;
+
+                        _ImageLoading = false;
+                        Raise(() => Image);
+                    });
+                }
+                return _img;
+            }
+            set { }
+        }
+        /// <summary>
+        /// Checks is default image.
+        /// </summary>
+        public bool IsDefault { get { return _isDefault; } }
+
         #endregion
 
         #region Static Methods
+
+        /// <summary>
+        /// Gets.
+        /// </summary>
+        /// <param name="partyName">The party name filter.</param>
+        /// <param name="pageNo">The Page No.</param>
+        /// <param name="rowPerPage">The Row per page.</param>
+        /// <returns>Returns list of MParty.</returns>
+        public static NDbResult<List<MParty>> Gets(string partyName,
+            int pageNo = 1, int rowPerPage = 10)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<List<MParty>> rets = new NDbResult<List<MParty>>();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                rets.ErrNum = 8000;
+                rets.ErrMsg = msg;
+
+                return rets;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@PartyName", partyName);
+
+            p.Add("@pageNum", value: pageNo, dbType: DbType.Int32, direction: ParameterDirection.InputOutput);
+            p.Add("@rowsPerPage", value: rowPerPage, dbType: DbType.Int32, direction: ParameterDirection.InputOutput);
+            p.Add("@totalRecords", value: 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@maxPage", value: 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                var items = cnn.Query<MParty>("GetMParties", p,
+                    commandType: CommandType.StoredProcedure);
+                rets.Value = (null != items) ? items.ToList() : null;
+
+                // Get Paging parameters
+                rets.PageNo = p.Get<int>("@pageNum");
+                rets.RowsPerPage = p.Get<int>("@rowsPerPage");
+                rets.TotalRecords = p.Get<int>("@totalRecords");
+                rets.MaxPage = p.Get<int>("@maxPage");
+                // Set error number/message
+                rets.ErrNum = p.Get<int>("@errNum");
+                rets.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                rets.ErrNum = 9999;
+                rets.ErrMsg = ex.Message;
+            }
+
+            if (null == rets.Value)
+            {
+                // create empty list.
+                rets.Value = new List<MParty>();
+            }
+
+            return rets;
+        }
 
         #endregion
     }
