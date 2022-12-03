add5e00edb660d67d7e4ffe7ea9bd53e6542da31
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-03 23:38:53 +0700

    Election.Analytic.System.v2 - PPRP  #246 - PPRP.Manangement.App project   - Implements UI #135.

---
+++ 09.App/PPRP.Manangement.App/Windows/Party/PartyEditorWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+using NLib;
+using NLib.Reflection;
+using NLib.Services;
+
+using PPRP.Models;
+
+#endregion
+
+
+namespace PPRP.Windows
+{
+    /// <summary>
+    /// Interaction logic for PartyEditorWindow.xaml
+    /// </summary>
+    public partial class PartyEditorWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PartyEditorWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdChangeIamge_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeImage();
+        }
+
+        private void cmdSave_Click(object sender, RoutedEventArgs e)
+        {
+            Save();
+        }
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ChangeImage()
+        {
+
+        }
+
+        private void Save()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+
+        }
+
+        #endregion
+    }
+}
