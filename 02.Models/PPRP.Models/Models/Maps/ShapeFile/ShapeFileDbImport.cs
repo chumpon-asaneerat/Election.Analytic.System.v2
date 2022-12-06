a7ade518099ef069b40205c055293ac3ddadbad9
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-06 23:20:57 +0700

    Election.Analytic.System.v2 - PPRP  #322 - PPRP.Manangement.App project   - Implements Map system #17.

--- 02.Models/PPRP.Models/Models/Maps/ShapeFile/ShapeFileDbImport.cs
+++ 02.Models/PPRP.Models/Models/Maps/ShapeFile/ShapeFileDbImport.cs
@@ -195,6 +195,7 @@ namespace PPRP.Services
                             var admPart = LADM1Part.Get(row.ADM0Code, row.ADM1Code, recordId).Value();
                             if (null == admPart) admPart = new LADM1Part();
                             admPart.ADM0Code = row.ADM0Code;
+                            admPart.ADM1Code = row.ADM1Code;
                             admPart.RecordId = recordId;
                             admPart.PointCount = part.Length;
                             // Save part
@@ -207,6 +208,7 @@ namespace PPRP.Services
                                 var admPoint = LADM1Point.Get(row.ADM0Code, row.ADM1Code, recordId, iCnt).Value();
                                 if (null == admPoint) admPoint = new LADM1Point();
                                 admPoint.ADM0Code = row.ADM0Code;
+                                admPoint.ADM1Code = row.ADM1Code;
                                 admPoint.RecordId = recordId;
                                 admPoint.PointId = iCnt;
                                 admPoint.X = point.X;
@@ -279,6 +281,74 @@ namespace PPRP.Services
             var recordId = shape.RecordNumber; // shape number
             var boundRect = new Models.RectangleD();
 
+            // cast shape based on the type
+            switch (shape.Type)
+            {
+                case ShapeType.Polygon:
+                    // a polygon contains one or more parts - each part is a list of points which
+                    // are clockwise for boundaries and anti-clockwise for holes 
+                    // see http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf
+                    ShapePolygon shapePolygon = shape as ShapePolygon;
+                    {
+                        int iPart = 1;
+                        int maxPart = shapePolygon.Parts.Count;
+                        foreach (PointD[] part in shapePolygon.Parts)
+                        {
+                            ShapeMapDbService.Instance.Db.BeginTransaction();
+
+
+                            var admPart = LADM2Part.Get(row.ADM0Code, row.ADM1Code, row.ADM2Code, recordId).Value();
+                            if (null == admPart) admPart = new LADM2Part();
+                            admPart.ADM0Code = row.ADM0Code;
+                            admPart.ADM1Code = row.ADM1Code;
+                            admPart.RecordId = recordId;
+                            admPart.PointCount = part.Length;
+                            // Save part
+                            LADM2Part.Save(admPart);
+
+                            int iCnt = 1;
+                            int maxPts = part.Length;
+                            foreach (PointD point in part)
+                            {
+                                var admPoint = LADM2Point.Get(row.ADM0Code, row.ADM1Code, row.ADM2Code, recordId, iCnt).Value();
+                                if (null == admPoint) admPoint = new LADM2Point();
+                                admPoint.ADM0Code = row.ADM0Code;
+                                admPoint.ADM1Code = row.ADM1Code;
+                                admPoint.RecordId = recordId;
+                                admPoint.PointId = iCnt;
+                                admPoint.X = point.X;
+                                admPoint.Y = point.Y;
+                                // Save point
+                                LADM2Point.Save(admPoint);
+
+                                // Set Bound Rect
+                                if (boundRect.Left == 0 || point.X < boundRect.Left) boundRect.Left = point.X;
+                                if (boundRect.Right == 0 || point.X > boundRect.Right) boundRect.Right = point.X;
+                                if (boundRect.Top == 0 || point.Y < boundRect.Top) boundRect.Top = point.Y;
+                                if (boundRect.Bottom == 0 || point.Y > boundRect.Bottom) boundRect.Bottom = point.Y;
+
+                                if (null != action)
+                                {
+                                    action(shape.RecordNumber, shapeCount, iPart, maxPart, iCnt, maxPts);
+                                }
+
+                                iCnt++;
+                            }
+
+                            ShapeMapDbService.Instance.Db.Commit();
+
+                            iPart++;
+                        }
+                    }
+                    break;
+                default:
+                    // other not supports.
+                    if (null != action)
+                    {
+                        action(shape.RecordNumber, shapeCount, 0, 0, 0, 0);
+                    }
+                    break;
+            }
 
             // Set Bound Rect
             row.LF = boundRect.Left;
@@ -316,6 +386,74 @@ namespace PPRP.Services
             var recordId = shape.RecordNumber; // shape number
             var boundRect = new Models.RectangleD();
 
+            // cast shape based on the type
+            switch (shape.Type)
+            {
+                case ShapeType.Polygon:
+                    // a polygon contains one or more parts - each part is a list of points which
+                    // are clockwise for boundaries and anti-clockwise for holes 
+                    // see http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf
+                    ShapePolygon shapePolygon = shape as ShapePolygon;
+                    {
+                        int iPart = 1;
+                        int maxPart = shapePolygon.Parts.Count;
+                        foreach (PointD[] part in shapePolygon.Parts)
+                        {
+                            ShapeMapDbService.Instance.Db.BeginTransaction();
+
+
+                            var admPart = LADM3Part.Get(row.ADM0Code, row.ADM1Code, row.ADM2Code, row.ADM3Code, recordId).Value();
+                            if (null == admPart) admPart = new LADM3Part();
+                            admPart.ADM0Code = row.ADM0Code;
+                            admPart.ADM1Code = row.ADM1Code;
+                            admPart.RecordId = recordId;
+                            admPart.PointCount = part.Length;
+                            // Save part
+                            LADM3Part.Save(admPart);
+
+                            int iCnt = 1;
+                            int maxPts = part.Length;
+                            foreach (PointD point in part)
+                            {
+                                var admPoint = LADM3Point.Get(row.ADM0Code, row.ADM1Code, row.ADM2Code, row.ADM3Code, recordId, iCnt).Value();
+                                if (null == admPoint) admPoint = new LADM3Point();
+                                admPoint.ADM0Code = row.ADM0Code;
+                                admPoint.ADM1Code = row.ADM1Code;
+                                admPoint.RecordId = recordId;
+                                admPoint.PointId = iCnt;
+                                admPoint.X = point.X;
+                                admPoint.Y = point.Y;
+                                // Save point
+                                LADM3Point.Save(admPoint);
+
+                                // Set Bound Rect
+                                if (boundRect.Left == 0 || point.X < boundRect.Left) boundRect.Left = point.X;
+                                if (boundRect.Right == 0 || point.X > boundRect.Right) boundRect.Right = point.X;
+                                if (boundRect.Top == 0 || point.Y < boundRect.Top) boundRect.Top = point.Y;
+                                if (boundRect.Bottom == 0 || point.Y > boundRect.Bottom) boundRect.Bottom = point.Y;
+
+                                if (null != action)
+                                {
+                                    action(shape.RecordNumber, shapeCount, iPart, maxPart, iCnt, maxPts);
+                                }
+
+                                iCnt++;
+                            }
+
+                            ShapeMapDbService.Instance.Db.Commit();
+
+                            iPart++;
+                        }
+                    }
+                    break;
+                default:
+                    // other not supports.
+                    if (null != action)
+                    {
+                        action(shape.RecordNumber, shapeCount, 0, 0, 0, 0);
+                    }
+                    break;
+            }
 
             // Set Bound Rect
             row.LF = boundRect.Left;
