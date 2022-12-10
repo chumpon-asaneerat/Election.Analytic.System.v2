23cad122bb27f8b01e1cf6678e3557d5ef332e49
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-10 19:16:51 +0700

    Election.Analytic.System.v2 - PPRP  #396 - PPRP.Manangement.App project   - Merge shape file related class in to single file #6.

--- 02.Models/PPRP.Models/Models/Maps/ShapeFile/Shapefile.cs
+++ 02.Models/PPRP.Models/Models/Maps/ShapeFile/Shapefile.cs
@@ -389,6 +389,136 @@ namespace PPRP.Models.ShapeFiles
 
     #endregion
 
+    #region ShapePoint
+
+    /// <summary>
+    /// A Shapefile Point Shape
+    /// </summary>
+    public class ShapePoint : Shape
+    {
+        #region Constructor
+
+        /// <summary>
+        /// A Shapefile Point Shape
+        /// </summary>
+        /// <param name="recordNumber">The record number in the Shapefile</param>
+        /// <param name="metadata">Metadata about the shape</param>
+        /// <param name="dataRecord">IDataRecord associated with the metadata</param>
+        /// <param name="shapeData">The shape record as a byte array</param>
+        /// <exception cref="ArgumentNullException">Thrown if shapeData is null</exception>
+        /// <exception cref="InvalidOperationException">Thrown if an error occurs parsing shapeData</exception>
+        protected internal ShapePoint(int recordNumber, StringDictionary metadata, IDataRecord dataRecord, byte[] shapeData)
+            : base(ShapeType.Point, recordNumber, metadata, dataRecord)
+        {
+            // metadata is validated by the base class
+            if (shapeData == null)
+            {
+                throw new ArgumentNullException("shapeData");
+            }
+
+            // Note, shapeData includes an 8 byte header so positions below are +8
+            // Position     Field       Value   Type        Number  Order
+            // Byte 0       Shape Type  1       Integer     1       Little
+            // Byte 4       X           X       Double      1       Little
+            // Byte 12      Y           Y       Double      1       Little
+
+            // validation - shapedata should be 8 + 4 + 8 + 8 = 28 bytes long
+            if (shapeData.Length != 28)
+            {
+                throw new InvalidOperationException("Invalid shape data");
+            }
+
+            Point = new PointD(EndianBitConverter.ToDouble(shapeData, 12, ProvidedOrder.Little),
+                EndianBitConverter.ToDouble(shapeData, 20, ProvidedOrder.Little));
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>Gets the point</summary>
+        public PointD Point { get; }
+
+        #endregion
+    }
+
+    #endregion
+
+    #region ShapeMultiPoint
+
+    /// <summary>
+    /// A Shapefile MultiPoint Shape
+    /// </summary>
+    public class ShapeMultiPoint : Shape
+    {
+        #region Constructor
+
+        /// <summary>
+        /// A Shapefile MultiPoint Shape
+        /// </summary>
+        /// <param name="recordNumber">The record number in the Shapefile</param>
+        /// <param name="metadata">Metadata about the shape</param>
+        /// <param name="dataRecord">IDataRecord associated with the metadata</param>
+        /// <param name="shapeData">The shape record as a byte array</param>
+        /// <exception cref="ArgumentNullException">Thrown if shapeData is null</exception>
+        /// <exception cref="InvalidOperationException">Thrown if an error occurs parsing shapeData</exception>
+        protected internal ShapeMultiPoint(int recordNumber, StringDictionary metadata
+            , IDataRecord dataRecord, byte[] shapeData)
+            : base(ShapeType.MultiPoint, recordNumber, metadata, dataRecord)
+        {
+            // metadata is validated by the base class
+            if (shapeData == null)
+            {
+                throw new ArgumentNullException("shapeData");
+            }
+
+            // Note, shapeData includes an 8 byte header so positions below are +8
+            // Position     Field       Value       Type        Number      Order
+            // Byte 0       Shape Type  8           Integer     1           Little
+            // Byte 4       Box         Box         Double      4           Little
+            // Byte 36      NumPoints   Num Points  Integer     1           Little
+            // Byte 40      Points      Points      Point       NumPoints   Little
+
+            // validation step 1 - must have at least 8 + 4 + (4*8) + 4 bytes = 48
+            if (shapeData.Length < 48)
+            {
+                throw new InvalidOperationException("Invalid shape data");
+            }
+
+            // extract bounding box and points
+            BoundingBox = ParseBoundingBox(shapeData, 12, ProvidedOrder.Little);
+            int numPoints = EndianBitConverter.ToInt32(shapeData, 44, ProvidedOrder.Little);
+
+            // validation step 2 - we're expecting 16 * numPoints + 48 bytes total
+            if (shapeData.Length != 48 + (16 * numPoints))
+            {
+                throw new InvalidOperationException("Invalid shape data");
+            }
+
+            // now extract the points
+            Points = new PointD[numPoints];
+            for (int pointNum = 0; pointNum < numPoints; pointNum++)
+            {
+                Points[pointNum] = new PointD(EndianBitConverter.ToDouble(shapeData, 48 + (16 * pointNum), ProvidedOrder.Little),
+                    EndianBitConverter.ToDouble(shapeData, 56 + (16 * pointNum), ProvidedOrder.Little));
+            }
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>Gets the bounding box</summary>
+        public RectangleD BoundingBox { get; }
+        /// <summary>Gets the array of points</summary>
+        public PointD[] Points { get; }
+
+        #endregion
+    }
+
+    #endregion
+
     #endregion
 
     #region FileFormat classes
