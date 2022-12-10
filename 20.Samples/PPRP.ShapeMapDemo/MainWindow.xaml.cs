2b99b69d6c816c29389b64ed5ff1fde4343e1111
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-10 19:38:39 +0700

    Election.Analytic.System.v2 - PPRP  #417 - PPRP - ShapeMapDemo project.   - ShapeMapDemo project Update #16

--- 20.Samples/PPRP.ShapeMapDemo/MainWindow.xaml.cs
+++ 20.Samples/PPRP.ShapeMapDemo/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+#endregion
+
 namespace PPRP
 {
     /// <summary>
@@ -20,9 +24,16 @@ namespace PPRP
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #endregion
     }
 }
