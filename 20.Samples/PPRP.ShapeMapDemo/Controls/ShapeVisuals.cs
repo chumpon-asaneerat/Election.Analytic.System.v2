3c471321f37576830be4af332ea8032bbacfec4b
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-10 19:42:08 +0700

    Election.Analytic.System.v2 - PPRP  #421 - PPRP - ShapeMapDemo project.   - ShapeMapDemo project Update #19

--- 20.Samples/PPRP.ShapeMapDemo/Controls/ShapeVisuals.cs
+++ 20.Samples/PPRP.ShapeMapDemo/Controls/ShapeVisuals.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+using PPRP.Models.ShapeFiles;
+using System.Windows.Threading;
+
+#endregion
 
 namespace PPRP.Controls
 {
     class ShapeVisuals
     {
+    }
+
+    public class TileMapVisual : UIElement
+    {
+
     }
 }
