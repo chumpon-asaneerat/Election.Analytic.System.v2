9fd5b7b4ae5e636fb3276a6bd9e2215203271b1a
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-10 19:12:59 +0700

    Election.Analytic.System.v2 - PPRP  #393 - PPRP.Manangement.App project   - Merge shape file related class in to single file #3.

--- 02.Models/PPRP.Models/Models/Maps/ShapeFile/Shapefile.cs
+++ 02.Models/PPRP.Models/Models/Maps/ShapeFile/Shapefile.cs
@@ -18,6 +18,86 @@ using System.Drawing;
 
 namespace PPRP.Models.ShapeFiles
 {
+    #region Structs
+
+    #region PointD Struct
+
+    /// <summary>
+    /// A simple double precision point
+    /// </summary>
+    public struct PointD
+    {
+        #region Public Fields
+
+        /// <summary>Gets or sets the X value</summary>
+        public double X;
+        /// <summary>Gets or sets the Y value</summary>
+        public double Y;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// A simple double precision point
+        /// </summary>
+        /// <param name="x">X value</param>
+        /// <param name="y">Y value</param>
+        public PointD(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        #endregion
+    }
+
+    #endregion
+
+    #region RectangleD Struct
+
+    /// <summary>
+    /// A simple double precision rectangle
+    /// </summary>
+    public struct RectangleD
+    {
+        #region Public Fields
+
+        /// <summary>Gets or sets the left value</summary>
+        public double Left;
+        /// <summary>Gets or sets the top value</summary>
+        public double Top;
+        /// <summary>Gets or sets the right value</summary>
+        public double Right;
+        /// <summary>Gets or sets the bottom value</summary>
+        public double Bottom;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// A simple double precision rectangle
+        /// </summary>
+        /// <param name="left">Left</param>
+        /// <param name="top">Top</param>
+        /// <param name="right">Right</param>
+        /// <param name="bottom">Bottom</param>
+        public RectangleD(double left, double top, double right, double bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        #endregion
+    }
+
+    #endregion
+
+    #endregion
+
     #region EndianBitConverter classes
 
     #region ProvidedOrder (Enum)
