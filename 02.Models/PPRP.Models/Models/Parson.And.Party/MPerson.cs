6d4740a7dc6cc768a022ced6251884d9ac9e9bb8
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-13 07:22:44 +0700

    Election.Analytic.System.v2 - PPRP  #440 - PPRP.Manangement.App project   - Implements Map system #84.

--- 02.Models/PPRP.Models/Models/Parson.And.Party/MPerson.cs
+++ 02.Models/PPRP.Models/Models/Parson.And.Party/MPerson.cs
@@ -523,6 +523,54 @@ namespace PPRP.Models
 
             return rets;
         }
+        /// <summary>
+        /// Save Image.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static NDbResult SaveImage(MPerson value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult ret = new NDbResult();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@PersonId", value.PersonId);
+            p.Add("@Data", value.Data, dbType: DbType.Binary, direction: ParameterDirection.Input, size: -1);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("SaveMPersonImage", p, commandType: CommandType.StoredProcedure);
+                ret.Success();
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
 
         #endregion
     }
