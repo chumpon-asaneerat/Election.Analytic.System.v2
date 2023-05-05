2fd4820a05626acbf887540db8fcc40ecefcc6da
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-05 18:40:33 +0700

    Election.Analytic.System.v2 - PPRP  #635 - Implements MPDCOffifial #23

---
+++ 02.Models/PPRP.Models/Scripts/V002/01.UpdateMPDCOfficialVoteCount.sql
@@ -0,0 +1,54 @@
+﻿-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	UpdateMPDCOfficialVoteCount
+-- [== History ==]
+-- <2022-09-29> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+-- 
+-- -- =============================================
+CREATE PROCEDURE [dbo].[UpdateMPDCOfficialVoteCount] (
+  @ThaiYear int    
+, @ADM1Code nvarchar(20)
+, @PollingUnitNo int
+, @RevoteNo int = 0
+, @PartyId int
+, @PersonId int
+, @VoteCount int = 0
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+		IF (   @ThaiYear IS NULL 
+            OR @ADM1Code IS NULL 
+            OR @PollingUnitNo IS NULL 
+            OR @RevoteNo IS NULL
+            OR @PartyId IS NULL
+            OR @PersonId IS NULL
+           )
+		BEGIN
+			SET @errNum = 100;
+			SET @errMsg = 'Some parameter(s) is null';
+			RETURN
+		END
+
+        UPDATE MPDCOfficial
+           SET VoteCount = COALESCE(@VoteCount, VoteCount)
+        WHERE ThaiYear = @ThaiYear
+          AND UPPER(LTRIM(RTRIM(ADM1Code))) = UPPER(LTRIM(RTRIM(@ADM1Code)))
+          AND PollingUnitNo = @PollingUnitNo
+          AND RevoteNo = @RevoteNo
+          AND PartyId = @PartyId
+          AND PersonId = @PersonId
+
+		-- Update Error Status/Message
+		SET @errNum = 0;
+		SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END;
---
+++ 02.Models/PPRP.Models/Scripts/V002/02.GetMPDCOfficialTopVoteSummaries.sql
@@ -0,0 +1,59 @@
+﻿-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMPDCOfficialTopVoteSummaries
+-- [== History ==]
+-- <2022-09-29> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- =============================================
+ALTER PROCEDURE [dbo].[GetMPDCOfficialTopVoteSummaries]
+(
+  @ThaiYear int
+, @PrevThaiYear int
+, @ADM1Code nvarchar(20)
+, @PollingUnitNo int
+, @Top int = 6
+)
+AS
+BEGIN
+DECLARE @sqlCommand as nvarchar(MAX);
+    SET @sqlCommand = N'
+    ;WITH Top6VoteSum66 AS
+    (
+        SELECT A.ThaiYear
+		     , A.ProvinceId
+             , A.ADM1Code
+             , A.ProvinceNameTH
+             , A.PollingUnitNo
+             , A.FullName
+             , A.PartyName
+             , A.PartyId
+             , A.PartyImageData
+             , A.PersonId
+             , A.PersonImageData
+             , A.RankNo
+             , A.VoteCount
+             , A.RevoteNo
+             , A.SortOrder
+			 , B.ADM1Code AS PrevADM1Code
+			 , B.ProvinceNameTH AS PrevProvinceNameTH
+			 , B.PollingUnitNo AS PrevPollingUnitNo
+			 , B.PartyId AS PrevPartyId
+			 , B.PartyName AS PrevPartyName
+			 , B.VoteCount AS PrevVoteCount
+			 , B.RankNo AS PrevRankNo
+          FROM MPDCOfficialView A LEFT OUTER JOIN MPDVoteSummaryView B 
+		    ON B.PersonId = A.PersonId AND B.ThaiYear = ' + CONVERT(nvarchar, @PrevThaiYear) + '
+		 WHERE A.ThaiYear = ' + CONVERT(nvarchar, @ThaiYear) + '
+    )
+    SELECT TOP ' + CONVERT(nvarchar, @Top) + ' *
+      FROM Top6VoteSum66
+     WHERE ThaiYear = ' + CONVERT(nvarchar, @ThaiYear) + '
+       AND ADM1Code = N''' + @ADM1Code + '''
+       AND PollingUnitNo = ' + CONVERT(nvarchar, @PollingUnitNo) + '
+     ORDER BY VoteCount DESC, SortOrder ASC
+    ';
+    EXECUTE dbo.sp_executesql @sqlCommand
+END;
